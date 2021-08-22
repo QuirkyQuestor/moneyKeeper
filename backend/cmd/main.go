8f73dfd949086f447769ff12b188f5c907942386
Valery <valery@winchien.circus.net>
2021-08-21 21:29:16 -0400

    Using gorilla/mux to get URL path parameters Adding sql QueryRow to get single account type

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/winchien/moneyKeeper/backend/cmd/datamodel"
 	"github.com/winchien/moneyKeeper/backend/cmd/sqlhandler"
 )
@@ -58,7 +60,7 @@ func accountTypeHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
-		// Fake account
+		// Get accountType from DB
 		var accountTypes []datamodel.AccountType
 		bdStatement, err := DBConnection.Prepare("SELECT typeId, name, description FROM AccountType")
 		if err != nil {
@@ -111,15 +113,94 @@ func accountTypeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func accountTypeIDHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	log.Printf("accountTypeID to get is: %v", vars["id"])
+
+	switch r.Method {
+	case "GET":
+		// Get accountType from DB
+		var accountType datamodel.AccountType
+		bdStatement, err := DBConnection.Prepare("SELECT typeId, name, description FROM AccountType WHERE typeId = ?")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer bdStatement.Close()
+
+		var typeId int
+		var name string
+		var description sql.NullString
+
+		err = bdStatement.QueryRow(vars["id"]).Scan(&typeId, &name, &description)
+
+		if err != nil {
+			log.Print(err)
+			w.WriteHeader(http.StatusNotFound)
+			resp := make(map[string]string)
+			resp["message"] = fmt.Sprintf("AccountType with ID %v was not found", vars["id"])
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Printf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
+
+			return
+		}
+		accountType = datamodel.AccountType{
+			TypeID:      int64(typeId),
+			Name:        name,
+			Description: description.String,
+		}
+
+		j, _ := json.Marshal(accountType)
+		w.Write(j)
+	case "PUT":
+		// Decode the JSON in the body and overwrite 'tom' with it
+		d := json.NewDecoder(r.Body)
+		p := &datamodel.AccountType{}
+		err := d.Decode(p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// tom := p
+	case "DELETE":
+		// Decode the JSON in the body and overwrite 'tom' with it
+		d := json.NewDecoder(r.Body)
+		p := &datamodel.AccountType{}
+		err := d.Decode(p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// tom := p
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "I can't do that.")
+	}
+}
+
 func main() {
 	DBConnection = sqlhandler.DBConnect()
 
-	http.HandleFunc("/api/", apiHandler)                     // GET saying Hello
-	http.HandleFunc("/api/account", accountHandler)          // GET ALL accounts, GET single specified account, POST new account, PUT updates the account, DELETE specified account
-	http.HandleFunc("/api/account_type", accountTypeHandler) // GET ALL account types, GET single specified account type, POST new account type, PUT to update account type, DELETE specified account type
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/", apiHandler)            // GET saying Hello
+	r.HandleFunc("/api/account", accountHandler) // Handle Accout requests: GET ALL accounts, GET single specified account, POST new account, PUT updates the account, DELETE specified account
+
+	r.HandleFunc("/api/account_type", accountTypeHandler)               // Handle account type requests: GET all account type, POST new account type
+	r.HandleFunc("/api/account_type/{id:[0-9]+}", accountTypeIDHandler) // Handle account type requests: GET single specified account type, PUT to update account type, DELETE specified account type
+
 	// http.HandleFunc("/api/category", categoryTypeHandler)       // GET ALL categories, GET single category info, POST new category, PUT update category, DELETE category
 	// http.HandleFunc("/api/transaction", transactionTypeHandler) // GET ALL transactions, GET single transaction info, POST new transaction, PUT update transaction, DELETE transaction, SEARCH transactions by some criteria
 
 	log.Println("Go!")
-	http.ListenAndServe(":8080", nil)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
